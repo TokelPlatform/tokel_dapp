2d48584fe2c79273bda5c701019194090b981f95
piggydoughnut <drmikhailova@gmail.com>
2021-02-24 18:04:07 +1300

    global variables and fonts setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import '../assets/fonts/fonts.css';
+import './styles/global.css';
+import './styles/variables.css';
 
 render(<App />, document.getElementById('root'));
