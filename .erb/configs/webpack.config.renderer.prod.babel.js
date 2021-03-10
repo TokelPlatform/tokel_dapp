37f0d19dee2faa7de4500d3bb573243e7b8f5eff
piggydoughnut <drmikhailova@gmail.com>
2021-03-10 17:42:30 +1300

    prod config

--- .erb/configs/webpack.config.renderer.prod.babel.js
+++ .erb/configs/webpack.config.renderer.prod.babel.js
@@ -31,19 +31,19 @@ export default merge(baseConfig, {
     login: [
       'core-js',
       'regenerator-runtime/runtime',
-      path.join(__dirname, '..', '/src/components/Login/index')
+      path.join(__dirname, '..', '/../src/components/Login/index')
     ],
     dashboard: [
       'core-js',
       'regenerator-runtime/runtime',
-      path.join(__dirname, '..', 'app/components/Dashboard/index')
+      path.join(__dirname, '..', '/../src/components/Dashboard/index')
     ]
   },
 
   output: {
     path: path.join(__dirname, '../../src/dist'),
     publicPath: './dist/',
-    filename: 'renderer.prod.js',
+    filename: '[name].renderer.prod.js',
   },
 
   module: {
