b2d4f9dfd4b4d669d306f66d445bae810facf893
piggydoughnut <drmikhailova@gmail.com>
2021-04-20 19:40:16 +0800

    activity table initial style

--- src/components/Dashboard/widgets/ActivityTable.tsx
+++ src/components/Dashboard/widgets/ActivityTable.tsx
@@ -7,8 +7,46 @@ import { selectUnspentUtxos } from 'store/selectors';
 
 import { WidgetContainer, WidgetTitle } from './common';
 
+const chosenAsset = {
+  name: 'TKLTEST',
+  fiatValue: 3.5,
+};
+
 const ActivityTableRoot = styled(WidgetContainer)`
   grid-column: span 3;
+`;
+
+const Transactions = styled.div`
+  display: grid;
+  grid-template-columns: 30% 30% 40%;
+  padding-left: 28px;
+  padding-right: 28px;
+
+  .datetime {
+    color: var(--color-gray);
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px 0 0 0;
+  .txAmount {
+    text-align: right;
+    margin: 0 0 2px 0;
+  }
+  .info {
+    margin: 0 0 2px 0;
+  }
+  .additionalInfo {
+    color: var(--color-gray);
+    margin: 0;
+    font-size: var(--font-size-additional-p);
+  }
+`;
+
+const TransactionWrapper = styled.div`
+  border-top: 1px solid var(--color-almostBlack2);
 `;
 
 const ActivityTable = (): ReactElement => {
@@ -18,7 +56,27 @@ const ActivityTable = (): ReactElement => {
     <ActivityTableRoot>
       <WidgetTitle>Recent Activity</WidgetTitle>
       {utxos.map(tx => (
-        <p key={tx.txid}>Txid {tx.txid}</p>
+        <TransactionWrapper key={tx.txid}>
+          <Transactions>
+            <Column>
+              <p className="datetime" style={{ 'align-self': 'start' }}>
+                {tx.height}
+              </p>
+            </Column>
+            <Column>
+              <p className="info">{tx.vout ? 'Received' : 'Sent'}</p>
+              <p className="additionalInfo">{tx.vout ? 'Deposit' : 'Withdrawal'}</p>
+            </Column>
+            <Column>
+              <p className="info" style={{ 'text-align': 'right' }}>
+                {tx.value.toFixed(8)} {chosenAsset.name}
+              </p>
+              <p className="additionalInfo" style={{ 'text-align': 'right' }}>
+                ${(tx.value * chosenAsset.fiatValue).toFixed(8)}
+              </p>
+            </Column>
+          </Transactions>
+        </TransactionWrapper>
       ))}
     </ActivityTableRoot>
   );
