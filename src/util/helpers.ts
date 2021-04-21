813271a4159e40dacb1d32dbcacc988263bce284
acnebs <connor.cn@gmail.com>
2021-04-21 00:10:45 -0500

    Simple method for quickly making consistent responsive CSS layouts

---
+++ src/util/helpers.ts
@@ -0,0 +1,20 @@
+import { WindowSize } from 'vars/defines';
+
+interface ResponsiveType {
+  XL: string;
+  L: string;
+  M: string;
+  S: string;
+}
+
+const responsiveFactory = (property: string) =>
+  Object.entries(WindowSize).reduce(
+    (o, [name, size]) => ({ ...o, [name]: `@media (${property}: ${size}px)` }),
+    {}
+  ) as ResponsiveType;
+
+// eslint-disable-next-line import/prefer-default-export
+export const Responsive = {
+  above: responsiveFactory('min-width'),
+  below: responsiveFactory('max-width'),
+};
