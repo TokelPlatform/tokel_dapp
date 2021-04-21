a0a3e693fc41c2f379fb99ba9b1c945282cb6e49
piggydoughnut <drmikhailova@gmail.com>
2021-04-21 16:02:18 +0800

    asset type

--- src/store/models/wallet.ts
+++ src/store/models/wallet.ts
@@ -1,39 +1,17 @@
 import { createModel } from '@rematch/core';
 
-import { broadcast, spend } from 'util/nspvlib';
-
 import type { RootModel } from './models';
 
 export type Asset = {
   name: string;
   ticker: string;
-  balance: number;
-  usd_value: number;
+  amount: number;
+  price: number;
 };
 export interface WalletState {
   chosenAsset?: string;
   assets: Array<Asset>;
-  currentTx: {
-    id: string;
-    status: number;
-    error: string;
-  };
 }
-
-interface SpendArgs {
-  address: string;
-  amount: string;
-}
-
-const updateCurrTx = (state, key, value) => {
-  return {
-    ...state,
-    currentTx: {
-      ...state.currentTx,
-      [key]: value,
-    },
-  };
-};
 
 export default createModel<RootModel>()({
   state: {
@@ -42,55 +20,21 @@ export default createModel<RootModel>()({
       {
         name: 'Tokel Test',
         ticker: 'TKLTEST',
-        balance: 1.2,
-        usd_value: 3,
+        amount: 1.2,
+        price: 3,
       },
       {
-        name: 'Komodo',
-        ticker: 'KMD',
-        balance: 3.0002342,
-        usd_value: 4,
+        name: 'Tokel',
+        ticker: 'TKL',
+        amount: 3.0002342,
+        price: 4,
       },
     ],
-    currentTx: {
-      id: '',
-      status: 0,
-    },
   } as WalletState,
   reducers: {
     SET_CHOSEN_ASSET: (state, chosenAsset: string) => ({
       ...state,
       chosenAsset,
     }),
-    SET_CURRENT_TX_ID: (state, txid: string) => updateCurrTx(state, 'id', txid),
-    SET_CURRENT_TX_STATUS: (state, txstatus: number) => updateCurrTx(state, 'status', txstatus),
-    SET_CURRENT_TX_ERROR: (state, error: string) => updateCurrTx(state, 'error', error),
-  },
-  effects: {
-    async spend({ address, amount }: SpendArgs) {
-      this.SET_CURRENT_TX_ID(null);
-      this.SET_CURRENT_TX_ERROR(null);
-      return spend(address, amount)
-        .then(res => {
-          if (res.result === 'success' && res.hex) {
-            this.SET_CURRENT_TX_ID(res.txid);
-            return broadcast(res.hex);
-          }
-          return null;
-        })
-        .then(broadcasted => {
-          if (broadcasted) {
-            // retcode < 0 .. error, === 1 success
-            this.SET_CURRENT_TX_STATUS(Number(broadcasted.retcode === 1));
-          }
-
-          return null;
-        })
-        .catch(e => {
-          this.SET_CURRENT_TX_STATUS(-1);
-          this.SET_CURRENT_TX_ERROR(e.message);
-          console.log(e.message);
-        });
-    },
   },
 });
