f2756c6ad7240a65b908770116180f852df964ce
piggydoughnut <drmikhailova@gmail.com>
2021-06-03 16:25:44 +0800

    animation progress bar on the login screen

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -6,6 +6,7 @@ import { dispatch } from 'store/rematch';
 import { getNewAddress } from 'util/nspvlib';
 import { TOPBAR_HEIGHT } from 'vars/defines';
 
+import Spinner from 'components/_General/Spinner';
 import ConfirmString from './ConfirmString';
 import GeneratedCredential from './GeneratedCredentials';
 import LoginForm from './LoginForm';
@@ -24,15 +25,31 @@ const LoginRoot = styled.div`
   }
 `;
 
+type ProgressProps = {
+  width: string;
+};
+
+const ProgressBarAnimation = styled.div<ProgressProps>`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  height: 5px;
+  width: ${props => props.width ?? '0%'};
+  background: var(--gradient-purple-horizontal);
+  transition: 0.3s;
+`;
+
 const STEP1 = 1;
 const STEP2 = 2;
 const STEP3 = 3;
 const STEP4 = 4;
+const STEP5 = 5;
 
 const Login = () => {
   const [step, setStep] = useState(STEP1);
   const [key, setKey] = useState(null);
   const [seed, setSeed] = useState(null);
+  const [showSpinner, setShowSpinner] = useState(false);
 
   useEffect(() => {
     if (key && seed) {
@@ -51,6 +68,8 @@ const Login = () => {
   const forward = () => setStep(step + 1);
   return (
     <LoginRoot>
+      {console.log(step)}
+      <ProgressBarAnimation width={`${(step - 1) * 25}%`} />
       {step === STEP1 && <LoginForm addNewWallet={() => forward()} />}
       {step === STEP2 && (
         <GeneratedCredential
@@ -69,15 +88,20 @@ const Login = () => {
           forward={() => forward()}
         />
       )}
-      {step === STEP4 && (
+      {(step === STEP4 || step === STEP5) && (
         <ConfirmString
           title="Confirm Your Seed Phrase"
           desc="You will use your seed phrase in case you need to restore access to your account. Please confirm it."
           originalString={seed}
           goBack={() => back()}
-          forward={() => dispatch.account.login({ key, setError: console.log })}
+          forward={() => {
+            forward();
+            setShowSpinner(true);
+            dispatch.account.login({ key, setError: console.log });
+          }}
         />
       )}
+      <div style={{ height: '30px' }}>{showSpinner && <Spinner />}</div>
     </LoginRoot>
   );
 };
