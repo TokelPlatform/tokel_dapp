939c3a4fdbc54c838c7b2f27afb8d3c691860dc2
piggydoughnut <drmikhailova@gmail.com>
2021-10-25 16:06:11 +0800

    github actions

--- src/components/Modal/content/Send.tsx
+++ src/components/Modal/content/Send.tsx
@@ -4,14 +4,7 @@ import { useSelector } from 'react-redux';
 import styled from '@emotion/styled';
 
 import { dispatch } from 'store/rematch';
-import {
-  selectAccountAddress,
-  selectChosenAsset,
-  selectCurrentTxError,
-  selectCurrentTxId,
-  selectCurrentTxStatus,
-  selectModalOptions,
-} from 'store/selectors';
+import { selectAccountAddress, selectChosenAsset, selectModalOptions } from 'store/selectors';
 import { BitgoAction, sendToBitgo } from 'util/bitgoHelper';
 import { ResourceType } from 'vars/defines';
 
@@ -32,9 +25,6 @@ const Send = () => {
   const options = useSelector(selectModalOptions) as SendModalOpts;
   const chosenAsset = useSelector(selectChosenAsset);
   const myAddress = useSelector(selectAccountAddress);
-  const currentTxId = useSelector(selectCurrentTxId);
-  const currentTxErorr = useSelector(selectCurrentTxError);
-  const currentTxStatus = useSelector(selectCurrentTxStatus);
 
   const [confirmation, setConfirmation] = useState(false);
   const [recipient, setRecipient] = useState(null);
