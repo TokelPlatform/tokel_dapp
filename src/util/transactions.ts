00e0288fd341ef0f1208a3f4a7e62823143f62c8
piggydoughnut <drmikhailova@gmail.com>
2021-11-17 12:43:47 +0800

    removed unused parsing functions, fixed typo in recipients, text label change

--- src/util/transactions.ts
+++ src/util/transactions.ts
@@ -1,87 +1,15 @@
 import { toBitcoin } from 'satoshi-bitcoin';
 
-import { FEE, INFORMATION_N_A, TICKER, USD_VALUE } from 'vars/defines';
-
-import { getTransactionDetail } from './insightApi';
-import { getRecepients, getSenders, groupTransactions } from './transactionsHelper';
-
-/**
- * Parse one transaction
- * @param tx
- * @returns
- */
-export const parseListTxsRpcTx = tx => {
-  if (tx.length === 2) {
-    return [
-      {
-        txid: tx[0].txid,
-        height: tx[0].height,
-        value: -(tx[0].value + (tx[1].value + FEE)),
-        received: false,
-        recepient: INFORMATION_N_A,
-      },
-    ];
-  }
-  // sending to yourself
-  if (tx.length === 3) {
-    return [
-      {
-        ...tx[0],
-        received: true,
-      },
-      {
-        ...tx[0],
-        received: false,
-        recepient: INFORMATION_N_A,
-      },
-    ];
-  }
-  // it is an incoming transaction, save as is
-  return [
-    {
-      ...tx[0],
-      value: Math.abs(tx[0].value),
-      received: true,
-    },
-  ];
-};
+import { TICKER, USD_VALUE } from 'vars/defines';
 
 export const parseBlockchainTransaction = (tx, address: string) => {
   const iAmSender = tx.senders.find(s => s === address);
+  console.log(tx);
   return [
     {
       value: toBitcoin(tx.value.toString()),
       from: tx.senders,
-      recipient: tx.recepients,
-      timestamp: tx.time,
-      txid: tx.txid,
-      height: tx.blockheight,
-      received: !iAmSender,
-    },
-  ];
-};
-
-/**
- * tx samples https://kmd.explorer.dexstats.info/insight-api-komodo/tx/b0fd208b3653ddeced4eabc5af6d3f50442cf0b5d59c34db79b4091b3163d578
- * @param tx        {object}   komodo insight api json object
- * @param address   {string}   current wallet address
- * @returns
- */
-export const parseSerializedTransaction = (tx, address) => {
-  if (tx.unconfirmed) {
-    return tx;
-  }
-  if (!tx.time) {
-    return parseListTxsRpcTx(tx);
-  }
-  const recipients = getRecepients(tx);
-  const senders = getSenders(tx);
-  const iAmSender = senders.find(s => s === address);
-  return [
-    {
-      value: Number(tx.vout[0].value),
-      from: senders,
-      recipient: iAmSender ? recipients : [address],
+      recipient: tx.recipients,
       timestamp: tx.time,
       txid: tx.txid,
       height: tx.blockheight,
@@ -123,16 +51,4 @@ export const parseUnspent = balance => {
       usd_value: USD_VALUE,
     },
   ];
-};
-
-/**
- * Gets details of all transactions
- * @param txs
- * @returns
- */
-export const getAllTransactionDetails = async txs => {
-  const grouppedTxs = groupTransactions(txs);
-  const txids = Object.keys(grouppedTxs);
-  const promises = txids.map(tx => getTransactionDetail(tx, grouppedTxs[tx]));
-  return Promise.all(promises);
 };
