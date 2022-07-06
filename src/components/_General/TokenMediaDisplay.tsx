e791d084aa48b2e4a7ba105c1e327daba80af42f
rannyeribaptist <rannyerirbatista@gmail.com>
2022-07-06 12:00:13 -0300

    whitelist of images by token ID

--- src/components/_General/TokenMediaDisplay.tsx
+++ src/components/_General/TokenMediaDisplay.tsx
@@ -5,7 +5,12 @@ import { ipcRenderer } from 'electron';
 
 import { Responsive, extractIPFSHash } from 'util/helpers';
 import { V } from 'util/theming';
-import { DEFAULT_IPFS_FALLBACK_GATEWAY, IPFS_IPC_ID, IpfsAction } from 'vars/defines';
+import {
+  DEFAULT_IPFS_FALLBACK_GATEWAY,
+  IPFS_IPC_ID,
+  IpfsAction,
+  TOKEN_WHITE_LIST_LOCATION,
+} from 'vars/defines';
 
 const MediaContent = styled.div`
   overflow-y: auto;
@@ -40,29 +45,38 @@ const TokenMediaDisplay: React.FC<TokenMediaDisplayProps> = ({ url }) => {
   const [mediaUrl, setMediaUrl] = useState(null);
 
   const ipfsId = useMemo(() => extractIPFSHash(url), [url]);
+  const tokenAddress = ipfsId || url;
+  const tokenInWhiteList =
+    JSON.parse(localStorage.getItem(`${TOKEN_WHITE_LIST_LOCATION}/${tokenAddress}`)) || false;
+
+  const [mediaShouldLoad, setMediaShouldLoad] = useState(tokenInWhiteList);
 
   useEffect(() => {
     setIframeHeight('unset');
     setMediaUrl(null);
+    setIframeLoaded(false);
+    setMediaShouldLoad(tokenInWhiteList);
     iframeRef.current?.contentWindow.postMessage({ mediaUrl: '', width: 0 });
-  }, [url]);
+  }, [url, tokenInWhiteList]);
 
   // Request IPFS file if it's an IPFS link. Set link meanwhile anyway
   useEffect(() => {
-    if (ipfsId) {
-      ipcRenderer.send(IPFS_IPC_ID, {
-        type: IpfsAction.GET,
-        payload: {
-          ipfsId,
-        },
-      });
+    if (iframeLoaded) {
+      if (ipfsId) {
+        ipcRenderer.send(IPFS_IPC_ID, {
+          type: IpfsAction.GET,
+          payload: {
+            ipfsId,
+          },
+        });
 
-      // Set fallback in case IPFS data never streams to our node
-      setMediaUrl(`${DEFAULT_IPFS_FALLBACK_GATEWAY}/${ipfsId}`);
-    } else {
-      setMediaUrl(url);
+        // Set fallback in case IPFS data never streams to our node
+        setMediaUrl(`${DEFAULT_IPFS_FALLBACK_GATEWAY}/${ipfsId}`);
+      } else {
+        setMediaUrl(url);
+      }
     }
-  }, [url, ipfsId]);
+  }, [url, ipfsId, iframeLoaded]);
 
   // Listen for IPFS files
   useEffect(() => {
@@ -107,19 +121,36 @@ const TokenMediaDisplay: React.FC<TokenMediaDisplayProps> = ({ url }) => {
     };
   }, [iframeRef, iframeLoaded]);
 
-  if (!mediaUrl) return null;
+  function addTokenToWhiteList() {
+    localStorage.setItem(`${TOKEN_WHITE_LIST_LOCATION}/${tokenAddress}`, 'true');
+    setMediaShouldLoad(true);
+  }
 
   return (
-    <MediaContent>
-      <ImageFrame>
-        <TokenMediaIframe
-          height={iframeHeight}
-          ref={iframeRef}
-          src={`file://${__dirname}/externalMedia.html`}
-          onLoad={() => setIframeLoaded(true)}
-        />
-      </ImageFrame>
-    </MediaContent>
+    <>
+      {mediaShouldLoad && (
+        <MediaContent>
+          <ImageFrame>
+            <TokenMediaIframe
+              height={iframeHeight}
+              ref={iframeRef}
+              src={`file://${__dirname}/externalMedia.html`}
+              onLoad={() => setIframeLoaded(true)}
+            />
+          </ImageFrame>
+        </MediaContent>
+      )}
+      {!mediaShouldLoad && (
+        <>
+          <button type="button" onClick={() => setMediaShouldLoad(true)}>
+            Load this time
+          </button>
+          <button type="button" onClick={() => addTokenToWhiteList()}>
+            Always load this token
+          </button>
+        </>
+      )}
+    </>
   );
 };
 
